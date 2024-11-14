ba1a2923f0ee4bf49f772b4a2e6526b9c4f5ca42
FernandoFrancaDev <soufadofernando@gmail.com>
2024-11-14 13:32:18 -0300

    Update ventilador DB

---
+++ ventilador/src/test/java/com/ventilador/ventilador/VentiladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventilador.ventilador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentiladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
