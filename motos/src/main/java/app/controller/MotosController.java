8f45bd0d534856f355bce698735c49c9cbccd787
FernandoFrancaDev <soufadofernando@gmail.com>
2024-11-20 12:43:42 -0300

    Upload Projeto Moto

---
+++ motos/src/main/java/app/controller/MotosController.java
@@ -0,0 +1,79 @@
+package app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.Motos;
+import app.service.MotosService;
+
+@RestController
+@RequestMapping("/api/moto")
+public class MotosController {
+	
+	@Autowired
+	private MotosService motosService;
+	
+	@PostMapping("/save")
+	public ResponseEntity<String> save(@RequestBody Motos motos){
+		
+		try {
+			
+			String mensagem = this.motosService.save(motos);
+			return new ResponseEntity<String>(mensagem, HttpStatus.OK);	
+			
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Algo deu errado", HttpStatus.OK);			
+			
+		}
+	}
+	@GetMapping("findById/{id}")
+	public ResponseEntity<Motos>findById(@PathVariable Long id){
+		
+		try {
+			Motos motos= this.motosService.findById(id);
+			return new ResponseEntity<>(motos, HttpStatus.OK);
+						
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+			
+		}
+	}
+	@GetMapping("/findByNome")
+	public ResponseEntity<List<Motos>> findByNome(@RequestParam String nome){
+		try {
+			List<Motos> lista = this.motosService.findByNome(nome);
+			return new ResponseEntity<>(lista, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+	@GetMapping("/findByMarca")
+	public ResponseEntity<List<Motos>> findByNome(@RequestParam long id){
+		try {
+			List<Motos> lista = this.motosService.findByMarca(id);
+			return new ResponseEntity<>(lista, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+	@GetMapping("/findAcimaAno")
+	public ResponseEntity<List<Motos>> findAcimaAno(@RequestParam int ano){
+		try {
+			List<Motos> lista = this.motosService.findAcimaAno(ano);
+			return new ResponseEntity<>(lista, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+}
