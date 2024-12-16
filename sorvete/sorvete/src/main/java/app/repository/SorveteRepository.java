40a281e763bc3bc2f84069221f4926ac32aa5d73
FernandoFrancaDev <soufadofernando@gmail.com>
2024-12-15 21:55:42 -0300

    Ajuste Projeto Sorvete

--- sorvete/sorvete/src/main/java/app/repository/SorveteRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Sorvete;
-
-public interface SorveteRepository extends JpaRepository<Sorvete, Long> {
-
-}
