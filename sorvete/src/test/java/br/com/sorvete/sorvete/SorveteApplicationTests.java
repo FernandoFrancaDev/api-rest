df4682a542b36ccaeee37e43ad3d6387cf862d7b
FernandoFrancaDev <soufadofernando@gmail.com>
2024-12-15 21:59:39 -0300

    upload Projeto Sorvete

---
+++ sorvete/src/test/java/br/com/sorvete/sorvete/SorveteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sorvete.sorvete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorveteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
